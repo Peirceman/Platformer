4d9aad8e58b3087fdec282f06a303c801890bffb
Peirceman <idris.peirce.mc@gmail.com>
2021-11-27 15:21:12 +0100

    add {} to menu init again

--- src/com/platformer/main/Menu.java
+++ src/com/platformer/main/Menu.java
@@ -19,72 +19,78 @@ public class Menu extends GamePanel {
 
     private void init() {
         // initialize panel
-        this.setSize(GamePanel.SCREEN_WIDTH, GamePanel.SCREEN_HEIGHT);
-        this.setLocation(0, 0);
-        this.setBackground(Color.WHITE);
-        this.setLayout(new GridBagLayout());
-        this.requestFocus();
+        {
+            this.setSize(GamePanel.SCREEN_WIDTH, GamePanel.SCREEN_HEIGHT);
+            this.setLocation(0, 0);
+            this.setBackground(Color.WHITE);
+            this.setLayout(new GridBagLayout());
+            this.requestFocus();
+        }
 
         // initialize tile text
-        JLabel title = new JLabel();
-        title.setFont(GamePanel.font);
-        title.setText("platformer");
-        title.setSize(metrics.stringWidth(title.getText()) + 2, 85);
-        title.setLocation((GamePanel.SCREEN_WIDTH - metrics.stringWidth(title.getText())) / 2, 0);
+        {
+            JLabel title = new JLabel();
+            title.setFont(GamePanel.font);
+            title.setText("platformer");
+            title.setSize(metrics.stringWidth(title.getText()) + 2, 85);
+            title.setLocation((GamePanel.SCREEN_WIDTH - metrics.stringWidth(title.getText())) / 2, 0);
 
-        this.gbc.insets = new Insets(15, 0, 15, 0);
-        this.gbc.gridx = 0;
-        this.gbc.gridy = 0;
-        this.add(title, this.gbc);
+            this.gbc.insets = new Insets(15, 0, 15, 0);
+            this.gbc.gridx = 0;
+            this.gbc.gridy = 0;
+            this.add(title, this.gbc);
+        }
 
         // initialize play button
-        JButton playButton = new JButton();
-        playButton.setFont(GamePanel.font);
-        playButton.setText("play");
-        playButton.setSize(new Dimension(225, 85));
-        playButton.setFocusable( false);
+        {
+            JButton playButton = new JButton();
+            playButton.setFont(GamePanel.font);
+            playButton.setText("play");
+            playButton.setSize(new Dimension(225, 85));
+            playButton.setFocusable(false);
 
-        playButton.addActionListener(e -> {
-            this.setLayout(null);
-            this.removeAll();
-            this.revalidate();
-            this.repaint();
+            playButton.addActionListener(e -> {
+                this.setLayout(null);
+                this.removeAll();
+                this.revalidate();
+                this.repaint();
 
-            JPanel inner = new JPanel(new FlowLayout(FlowLayout.CENTER, 12, 12));
-            inner.setSize(new Dimension(GamePanel.SCREEN_WIDTH - 100, GamePanel.SCREEN_HEIGHT - 100));
-            inner.setLocation((GamePanel.SCREEN_WIDTH - inner.getWidth()) / 2, (GamePanel.SCREEN_HEIGHT - inner.getHeight()) / 2);
-            inner.setBackground(Color.white);
+                JPanel inner = new JPanel(new FlowLayout(FlowLayout.CENTER, 12, 12));
+                inner.setSize(new Dimension(GamePanel.SCREEN_WIDTH - 100, GamePanel.SCREEN_HEIGHT - 100));
+                inner.setLocation((GamePanel.SCREEN_WIDTH - inner.getWidth()) / 2, (GamePanel.SCREEN_HEIGHT - inner.getHeight()) / 2);
+                inner.setBackground(Color.white);
 
-            for (int i = 1; i <= Game.LEVELS; i++) {
-                final int finalI = i;
+                for (int i = 1; i <= Game.LEVELS; i++) {
+                    final int finalI = i;
+                    final int size = Math.max(GamePanel.metrics.stringWidth(Integer.toString(i)), GamePanel.FONT_HEIGHT) + 2;
 
-                JButton button = new JButton();
-                button.setFont(GamePanel.font);
-                int maxSize = Math.max(metrics.stringWidth("" + i), metrics.getHeight()) + 2;
-                button.setPreferredSize(new Dimension(maxSize, maxSize));
-                button.setText("" + i);
-                button.addActionListener(ev -> Main.addGame(Integer.toString(finalI)));
+                    final JButton button = new JButton();
+                    button.setFont(GamePanel.font);
+                    button.setPreferredSize(new Dimension(size, size));
+                    button.setText(Integer.toString(finalI));
+                    button.addActionListener(ev -> Main.addGame(Integer.toString(finalI)));
 
-                inner.add(button);
-            }
+                    inner.add(button);
+                }
 
-            this.add(inner);
-            this.validate();
-        });
+                this.add(inner);
+                this.validate();
+            });
 
-        this.gbc.gridy = 1;
-        this.gbc.fill  = GridBagConstraints.BOTH;
-        this.add(playButton, this.gbc);
-
+            this.gbc.gridy = 1;
+            this.gbc.fill = GridBagConstraints.BOTH;
+            this.add(playButton, this.gbc);
+        }
         // initialize my levels button
-        JButton myLevels = new JButton();
-        myLevels.setFont(GamePanel.font);
-        myLevels.setText("my levels");
-        myLevels.setFocusable(false);
-        myLevels.addActionListener(e -> {
-
-        });
-        this.gbc.gridy = 2;
-        this.add(myLevels, this.gbc);
+        {
+            JButton myLevels = new JButton();
+            myLevels.setFont(GamePanel.font);
+            myLevels.setText("my levels");
+            myLevels.setFocusable(false);
+            myLevels.addActionListener(e -> {
+            });
+            this.gbc.gridy = 2;
+            this.add(myLevels, this.gbc);
+        }
     }
 }
